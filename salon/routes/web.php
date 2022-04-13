f45f74bacaafbcb6afe547cedf7fad79a5873cb9
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2022-04-13 11:53:41 +0530

    Made login and registration system

--- salon/routes/web.php
+++ salon/routes/web.php
@@ -15,8 +15,18 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get("/home",[HomeController::class,"index"]);
+Route::get("/",[HomeController::class,"index"]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
