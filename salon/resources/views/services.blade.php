27cee11e8a1b754120324ca761babeaae7e34568
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2022-04-13 14:28:57 +0530

    Creating Table & Model for services

---
+++ salon/resources/views/services.blade.php
@@ -0,0 +1,92 @@
+<!-- ***** Menu Area Starts ***** -->
+<section class="section" id="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="section-heading">
+                        <h6>Our Services</h6>
+                        <h2>Some of the most-used packages we bring.</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu-item-carousel">
+            <div class="col-lg-12">
+                <div class="owl-menu-item owl-carousel">
+                    <div class="item">
+                        <div class='card card1'>
+                            <div class="price"><h6>₹1000</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Hair-Color n' Care</h1>
+                              <p class='description'>Have Hair-color, Hair-smoothening, Hair-cut, Hair-wash, Straightning & Styling.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card2'>
+                            <div class="price"><h6>₹650</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Nail Care</h1>
+                              <p class='description'>We bring to you best medicure and pedicures and satisfactory nail art designs.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card3'>
+                            <div class="price"><h6>₹2500</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Bridal Set</h1>
+                              <p class='description'>On point styling with makeup and accessories for engagement, wedding & parties.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card4'>
+                            <div class="price"><h6>₹1500</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Makeup Kit</h1>
+                              <p class='description'>Makeup for any occasion with hair styling, booking can be done for groups.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card5'>
+                            <div class="price"><h6>₹500</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Mehndi</h1>
+                              <p class='description'>Decorate your festive season with mehndi or henna.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card3'>
+                        <div class="price"><h6>₹2500</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Bridal Set</h1>
+                              <p class='description'>On point styling with makeup and accessories for engagement, wedding & parties.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Menu Area Ends ***** -->
